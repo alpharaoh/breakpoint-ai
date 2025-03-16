d16ca82ea2e6b3f8c1c87d4acda1c53d63a22d19
alpharaoh <akaam@quantumboost.com>
2025-03-16 14:19:43 +0000

    Query

--- src/app/[thread]/page.tsx
+++ src/app/[thread]/page.tsx
@@ -1,30 +1,49 @@
+"use client";
 import { Assistant } from "@/app/assistant";
 
 import { ThreadMessageLike } from "@assistant-ui/react";
+import { usePathname } from "next/navigation";
 import { getThreadId } from "@/lib/get-thread-id";
 import { getThread } from "@/lib/chat-service";
 import { type Thread } from "@/lib/db";
-import { headers } from "next/headers";
+import { useEffect, useState } from "react";
 
-export default async function Thread() {
-  const headerList = await headers();
+export default function Thread() {
+  const pathname = usePathname();
+  const [currentThread, setCurrentThread] = useState<Thread>();
+  const [loading, setLoading] = useState<boolean>(true);
 
-  const pathname = headerList.get("x-current-path");
+  useEffect(() => {
+    const getThreadFunc = async () => {
+      const threadId = getThreadId(pathname);
+      try {
+        const thread = await getThread(threadId);
+        setCurrentThread(thread);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getThreadFunc();
+  }, [pathname]);
 
-  const threadId = getThreadId(pathname ?? "");
-  const thread = await getThread(threadId);
+  if (loading) {
+    return (
+      <div className="grid h-dvh grid-cols-[200px_1fr] gap-x-2 px-4 py-4">
+        Loading...
+      </div>
+    );
+  }
 
-  if (!thread) {
+  if (!currentThread) {
     return <div>Thread not found</div>;
   }
 
-  const initialMessages: ThreadMessageLike[] = thread.messages.map(
-    (message) => ({
+  const initialMessages: ThreadMessageLike[] =
+    currentThread?.messages?.map((message) => ({
       id: message.id,
       role: message.role as "user" | "assistant",
       content: JSON.parse(message.content),
-    }),
-  );
+    })) ?? [];
 
   return (
     <div className="bg-primary-foreground">
