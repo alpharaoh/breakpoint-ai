ad0cfb46314afdb5026a22a2d0686238ec67e1fa
alpharaoh <akaam@quantumboost.com>
2025-03-16 14:05:53 +0000

    Add loading state

--- src/app/[thread]/page.tsx
+++ src/app/[thread]/page.tsx
@@ -2,19 +2,37 @@
 import { Assistant } from "@/app/assistant";
 
 import { ThreadMessageLike } from "@assistant-ui/react";
-import { useLiveQuery } from "dexie-react-hooks";
 import { usePathname } from "next/navigation";
 import { getThreadId } from "@/lib/get-thread-id";
 import { getThread } from "@/lib/chat-service";
+import { type Thread } from "@/lib/db";
+import { useEffect, useState } from "react";
 
 export default function Thread() {
   const pathname = usePathname();
+  const [currentThread, setCurrentThread] = useState<Thread>();
+  const [loading, setLoading] = useState<boolean>(true);
 
-  const currentThread = useLiveQuery(() => {
-    const threadId = getThreadId(pathname);
+  useEffect(() => {
+    const getThreadFunc = async () => {
+      const threadId = getThreadId(pathname);
+      try {
+        const thread = await getThread(threadId);
+        setCurrentThread(thread);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getThreadFunc();
+  }, [pathname]);
 
-    return getThread(threadId);
-  });
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!currentThread) {
+    return <div>Thread not found</div>;
+  }
 
   const initialMessages: ThreadMessageLike[] =
     currentThread?.messages?.map((message) => ({
@@ -22,10 +40,6 @@ export default function Thread() {
       role: message.role as "user" | "assistant",
       content: JSON.parse(message.content),
     })) ?? [];
-
-  if (!currentThread) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="bg-primary-foreground">
